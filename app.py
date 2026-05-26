8a274094207c9d9080ae1272577c35b212216c8b
varshitha <varshitha@varshithas-MacBook-Pro-2.local>
2026-05-26 12:42:41 +0530

    phone camera working

--- app.py
+++ app.py
@@ -1,80 +1,10 @@
-from flask import Flask, render_template, Response
-import cv2
+from flask import Flask, render_template
 
 app = Flask(__name__)
-
-camera = cv2.VideoCapture(0)
-
-face = cv2.CascadeClassifier(
-    cv2.data.haarcascades +
-    "haarcascade_frontalface_default.xml"
-)
-
-def generate_frames():
-
-    while True:
-
-        success, frame = camera.read()
-
-        if not success:
-            break
-
-        gray = cv2.cvtColor(
-            frame,
-            cv2.COLOR_BGR2GRAY
-        )
-
-        faces = face.detectMultiScale(
-            gray,
-            1.3,
-            5
-        )
-
-        for (x, y, w, h) in faces:
-
-            cv2.rectangle(
-                frame,
-                (x, y),
-                (x+w, y+h),
-                (0,255,0),
-                3
-            )
-
-            cv2.putText(
-                frame,
-                "Face Detected",
-                (x, y-10),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                1,
-                (0,255,255),
-                2
-            )
-
-        ret, buffer = cv2.imencode(
-            ".jpg",
-            frame
-        )
-
-        frame = buffer.tobytes()
-
-        yield (
-            b'--frame\r\n'
-            b'Content-Type: image/jpeg\r\n\r\n'
-            + frame +
-            b'\r\n'
-        )
 
 @app.route("/")
 def home():
     return render_template("index.html")
-
-@app.route("/video")
-def video():
-
-    return Response(
-        generate_frames(),
-        mimetype="multipart/x-mixed-replace; boundary=frame"
-    )
 
 if __name__ == "__main__":
     app.run()
